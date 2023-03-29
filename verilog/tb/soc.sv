89027bc36de495b568d14cdb150fd43f9ad704e9
Taner Öksüz <taner.oeksuez@outlook.de>
2023-03-29 15:49:12 +0200

    systemc removed from design.

--- verilog/tb/soc.sv
+++ verilog/tb/soc.sv
@@ -1,8 +1,5 @@
-module soc
-(
-  input logic reset,
-  input logic clock
-);
+module soc;
+
   timeunit 1ns;
   timeprecision 1ps;
 
@@ -49,6 +46,18 @@ module soc
   logic [31 : 0] base_addr;
 
   logic [31 : 0] host[0:0] = '{default:'0};
+
+	logic reset = 0;
+	logic clock = 0;
+
+	initial begin
+		$timeformat(-9,0,"ns",0);
+		#10ns reset = 1;
+	end
+
+	always begin
+		#1ns clock=~clock;
+	end
 
   initial begin
     $readmemh("host.dat", host);
