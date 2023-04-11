a84e52e553b2cfcdb19a1d1d38551a31bca08a5f
Taner Öksüz <taner.oeksuez@outlook.de>
2023-04-11 15:39:50 +0200

    some errors fixed.

--- verilog/vivado/soc.sv
+++ verilog/vivado/soc.sv
@@ -123,7 +123,7 @@ module soc
           uart_valid = 0;
           clint_valid = 0;
           axi_valid = memory_valid;
-          base_addr = rom_base_addr;
+          base_addr = axi_base_addr;
       end else if (memory_addr >= clint_base_addr &&
         memory_addr < clint_top_addr) begin
           rom_valid = 0;
